2450509c0db93bb30eaf21914184caf16484f9f2
suzalvarezgomez <suzalvarezgomez@users.noreply.github.com>
2015-10-11 17:18:33 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -3,7 +3,7 @@ myData <- read.table("./Project1/household_power_consumption.txt", header=TRUE, 
 myWorkData <- myData[myData$Date %in% c("1/2/2007","2/2/2007") ,]
 
 # PREPARING CANVAS AND PLOTTING GRAPH 1 – VAR x1
-png("plot1.png", width=480, height=480, bg = "white")
+png("plot1.png", width=480, height=480, bg = NA)
 x1 <- myWorkData$Global_active_power
 hist( x1, col="#FF2500", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
